d1293a9a2c3ffea470177753bba4ecfcc5fee279
Kehinde13 <balogunkehinde3@gmail.com>
2023-07-16 04:42:04 -0700

    made the website responsive

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ let button = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", () => {
-        button[i].classList.toggle("active")
-     
-        let panel = button[i].nextElementSibling;
-        if(panel.style.maxHeight){
-            panel.style.maxHeight = null;
+        
+         if(button[i].classList.contains('active')) {
+            button[i].classList.remove('active')
+
         } else {
-            panel.style.maxHeight = panel.scrollHeight + 'px';
+            document.querySelector('.accordion.active')?.classList.remove('active')
+            button[i].classList.add('active')
         }
+        
     })
 }
