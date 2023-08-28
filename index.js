6ca0ab0692a47a1b0f59a51d34a7e929b410e2ef
Oluwasetemi Ojo <setemiojo@gmail.com>
2023-08-28 14:15:33 +0100

    update the trim Buttom

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const copyButton = document.querySelector('.copyButton');
 const alert = document.querySelector('#alert');
 const shortUrl = document.querySelector('#shortUrl');
 const trimButton = document.querySelector('#trimButton');
+const input = document.getElementById('Url')
+
+
 
 /* Accordion */
 for (let i = 0; i < button.length; i++) {
@@ -15,25 +18,38 @@ for (let i = 0; i < button.length; i++) {
     }
   });
 }
+// TODO: disable Trimbutton by default and enable it when input#Url has content more than 0
+input.onchange = function(event) {
+  let value = event.target.value;
+  if (value.length > 0) {
+    trimButton.setAttribute('disable', false)
+  }
+}
 
 /* Trimbutton */
 trimButton.addEventListener('click', async (e) => {
   e.preventDefault(e);
-  await urlShortener();
+  // get the input value
+  let longUrl = input?.value;
+
+  let shortenedUrl = await urlShortener(longUrl);
+  shortUrl.value = shortenedUrl;
 });
 
 /* URL Shortener */
-async function urlShortener() {
-  let longUrl = document.getElementById('Url').value;
-
-  const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${longUrl}`);
+async function urlShortener(value) {
+  if (!value) {
+    alert('Please pass in a valid url')
+    // TODO: Add a validation to check the value if it is a valid URL. Options (Regex or URL)
+    // https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+  }
+  const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${value}`);
   const data = await result.json();
-  shortUrl.value = data.result.short_link2;
+  return data?.result?.short_link2;
 }
 
 /* Copy Button */
 copyButton.addEventListener('click', () => {
-  // not sure line 39 is neccessary after 38.
   shortUrl.select();
   shortUrl.setSelectionRange(0, 99999);
 
