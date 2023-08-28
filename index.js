4ec272902754083bfc1a0b1f8028a35a5dfc7e70
Oluwasetemi Ojo <setemiojo@gmail.com>
2023-08-28 13:09:57 +0100

    formating and awaiting the urlShortener function

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const button = document.getElementsByClassName("accordion");
-const copyButton = document.querySelector(".copyButton");
-const alert = document.querySelector("#alert");
-const shortUrl = document.querySelector("#shortUrl");
-const trimButton = document.querySelector("#trimButton")
+const button = document.getElementsByClassName('accordion');
+const copyButton = document.querySelector('.copyButton');
+const alert = document.querySelector('#alert');
+const shortUrl = document.querySelector('#shortUrl');
+const trimButton = document.querySelector('#trimButton');
 
 /* Accordion */
 for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", () => {
-    if (button[i].classList.contains("active")) {
-      button[i].classList.remove("active");
+  button[i].addEventListener('click', () => {
+    if (button[i].classList.contains('active')) {
+      button[i].classList.remove('active');
     } else {
-      document.querySelector(".accordion.active")?.classList.remove("active");
-      button[i].classList.add("active");
+      document.querySelector('.accordion.active')?.classList.remove('active');
+      button[i].classList.add('active');
     }
   });
 }
 
 /* Trimbutton */
-trimButton.addEventListener('click', (e) => {
-    e.preventDefault(e)
-    UrlShortener()
-})
-
+trimButton.addEventListener('click', async (e) => {
+  e.preventDefault(e);
+  await urlShortener();
+});
 
 /* URL Shortener */
-async function UrlShortener() {
-  let longUrl = document.getElementById("Url").value;
+async function urlShortener() {
+  let longUrl = document.getElementById('Url').value;
 
   const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${longUrl}`);
   const data = await result.json();
@@ -33,7 +32,8 @@ async function UrlShortener() {
 }
 
 /* Copy Button */
-copyButton.addEventListener("click", () => {
+copyButton.addEventListener('click', () => {
+  // not sure line 39 is neccessary after 38.
   shortUrl.select();
   shortUrl.setSelectionRange(0, 99999);
 
